f760c4d3ead4410b7b4c457a5205bae22e47ce21
sexylowrie <sexylowrie@163.com>
2018-10-23 18:41:57 +0800

    首次提交

---
+++ src/main/java/com/yee/docker/DockerApplication.java
@@ -0,0 +1,20 @@
+package com.yee.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+	@RequestMapping("/docker")
+	public String docker(){
+		return "hello world";
+	}
+}
